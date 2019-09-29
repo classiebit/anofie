e13f5f473368e3a78bd7dc50143e7f09c759feaa
Deepak Panwar <dp.classiebit@gmail.com>
2019-09-29 19:48:35 +0530

    test demo mode on production

--- index.php
+++ index.php
@@ -89,6 +89,9 @@ if (strpos($domain, 'classiebit.com') !== FALSE || strpos($domain, 'anofie.test'
     define('DEMO_MODE', 1);
 else
     define('DEMO_MODE', 0);
+
+
+echo DEMO_MODE;exit;
 /* ------------- DEMO MODE ------------- */
 
 /*
